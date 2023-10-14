522938228cc58bbd3d5b1a9c12558e68f7fc17d5
Tran Minh Man <mantm040702@gmail.com>
2023-10-14 20:10:38 +0700

    fix some

--- green-craze-be-v1.Application/Model/Review/ReplyReviewRequest.cs
+++ green-craze-be-v1.Application/Model/Review/ReplyReviewRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace green_craze_be_v1.Application.Model.Review
 {
-    public class UpdateReviewRequest
+    public class ReplyReviewRequest
     {
         public string Reply { get; set; }
     }
