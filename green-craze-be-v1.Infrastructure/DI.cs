6e7d7d309b5a60e03f12b466bcbc64fc324f06b0
Nguyen Minh Son <nguyenminhson102002@gmail.com>
2023-12-01 17:44:43 +0700

    fix datetime

--- green-craze-be-v1.Infrastructure/DI.cs
+++ green-craze-be-v1.Infrastructure/DI.cs
@@ -11,63 +11,63 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace green_craze_be_v1.Infrastructure
 {
-	public static class DI
-	{
-		public static void AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
-		{
-			services.AddServices();
-			services.AddRepositories();
-			services.AddDbContextSetup(configuration);
-		}
+    public static class DI
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddServices();
+            services.AddRepositories();
+            services.AddDbContextSetup(configuration);
+        }
 
-		private static void AddRepositories(this IServiceCollection services)
-		{
-			services
-				.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork))
-				.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>))
-				.AddScoped<IStatisticRepository, StatisticRepository>();
-		}
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services
+                .AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork))
+                .AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>))
+                .AddScoped<IStatisticRepository, StatisticRepository>();
+        }
 
-		public static WebApplication MigrateDatabase(this WebApplication webApp)
-		{
-			using (var scope = webApp.Services.CreateScope())
-			{
-				using (var appContext = scope.ServiceProvider.GetRequiredService<AppDBContext>())
-				{
-					try
-					{
-						appContext.Database.Migrate();
-					}
-					catch (Exception ex)
-					{
-						throw;
-					}
-				}
-			}
-			return webApp;
-		}
+        public static WebApplication MigrateDatabase(this WebApplication webApp)
+        {
+            using (var scope = webApp.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<AppDBContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return webApp;
+        }
 
-		public static void AddDbContextSetup(this IServiceCollection services, IConfiguration configuration)
-		{
-			services.AddDbContext<AppDBContext>(options =>
-				options.UseMySQL(configuration.GetConnectionString("AppDBContext")));
-			services.AddIdentity<AppUser, AppRole>(opts =>
-			{
-				opts.Password.RequireNonAlphanumeric = false;
-				opts.Password.RequiredLength = 1;
-				opts.Password.RequireDigit = false;
-				opts.Password.RequireLowercase = false;
-				opts.Password.RequireUppercase = false;
-			})
-			.AddEntityFrameworkStores<AppDBContext>()
-			.AddDefaultTokenProviders();
-		}
+        public static void AddDbContextSetup(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDBContext>(options =>
+                options.UseMySQL(configuration.GetConnectionString("AppDBContext")));
+            services.AddIdentity<AppUser, AppRole>(opts =>
+            {
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequiredLength = 1;
+                opts.Password.RequireDigit = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+            })
+            .AddEntityFrameworkStores<AppDBContext>()
+            .AddDefaultTokenProviders();
+        }
 
-		private static void AddServices(this IServiceCollection services)
-		{
-			services
-				.AddScoped<IUploadService, UploadService>()
-				.AddScoped<IMailService, MailService>();
-		}
-	}
+        private static void AddServices(this IServiceCollection services)
+        {
+            services
+                .AddScoped<IUploadService, UploadService>()
+                .AddScoped<IMailService, MailService>();
+        }
+    }
 }
